63234a9f62a563ee08b3cc4394fffe8518c835ef
katherinenemits <59098985+katherinenemits@users.noreply.github.com>
2020-03-15 13:40:50 -0700

    Tutorial 5.3 Complete

--- Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,15 +13,22 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public bool isGameActive;
+    public Button restartButton;
 
     // Start is called before the first frame update
     void Start()
     {
+        
+
+    }
+
+    public void StartGame()
+    {
         StartCoroutine(SpawnTarget());
         score = 0;
-        scoreText.text = "Score: " + score; UpdateScore(0);
+        scoreText.text = "Score: " + score; 
+        UpdateScore(0);
         isGameActive = true;
-
     }
 
     // Update is called once per frame
@@ -49,7 +58,12 @@ public class GameManager : MonoBehaviour
     {
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+        restartButton.gameObject.SetActive(true);
     }
 
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
 
